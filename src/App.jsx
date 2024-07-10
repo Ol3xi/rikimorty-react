54213fd6b8d3e85e511bbcd78008f7213f6cd70b
olexitomaselli <olexitom29@gmail.com>
2024-07-10 18:35:02 +0200

    da inserire l'api del udemi e modificare le variabili con tours, modificare inoltre i nomi dei componenti.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import { Loading } from "./components/Loading/Loading";
+
+const url = "https://rickandmortyapi.com/api/character";
+
 export const App = () => {
+  const [loading, setIsLoading] = useState(true);
+  const [tours, setCharacters] = useState([]);
+  const fetchCharacters = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const characters = response.json();
+      console.log(characters);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  if (setIsLoading) {
+    return (
+      <span>
+        <Loading />
+      </span>
+    );
+  }
+
   return <div>App</div>;
 };
